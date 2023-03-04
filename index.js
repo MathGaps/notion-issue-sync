18137848b06138cd6bba2de468be04074ad15db1
RJM <rjmbowie@gmail.com>
2023-03-04 22:07:51 +1100

    feat: Init commit

--- index.js
+++ index.js
@@ -1,20 +1,51 @@
-const core = require('@actions/core');
-const wait = require('./wait');
-
+const core = require("@actions/core");
+const github = require("@actions/github");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const ms = core.getInput('milliseconds');
-    core.info(`Waiting ${ms} milliseconds ...`);
-
-    core.debug((new Date()).toTimeString()); // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
-    await wait(parseInt(ms));
-    core.info((new Date()).toTimeString());
-
-    core.setOutput('time', new Date().toTimeString());
+    console.info(
+      JSON.stringify(github.event),
+      JSON.stringify(github.context.repo)
+    );
+    const webhook = core.getInput("webhook");
+    const token = core.getInput("token");
+    const octokit = github.getOctokit(token);
+    const {
+      repository: {
+        closingIssuesReferences: { nodes: issues },
+      },
+    } = await octokit.graphql(
+      `
+    {
+      repository(owner: $owner, name: $name) {
+        closingIssuesReferences {
+          nodes {
+            body
+          }
+        }
+      }
+    }
+`,
+      {
+        owner: github.context.repo.owner,
+        name: github.context.repo.name,
+      }
+    );
+    await updateStateForIssues(webhook, issues);
   } catch (error) {
     core.setFailed(error.message);
+  }
+}
+
+async function updateStateForIssues(webhook, issues) {
+  for (const { body } of issues) {
+    const [_, databaseID] = body.split("ID: ");
+    fetch(webhook, {
+      body: JSON.stringify({
+        id: databaseID,
+      }),
+    });
   }
 }
 
